025dad479321855c458048833511839d1727f52c
Emma Olson <emma.olson794@gmail.com>
2022-08-22 11:49:36 -0500

    update landing page to map testimonial data and pass it to render cards

--- src/components/TestimonialCard/index.js
+++ src/components/TestimonialCard/index.js
@@ -4,17 +4,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-function TestimonialCard() {
+function TestimonialCard({ client, highlight, reviewText }) {
 
     const theme = useTheme();
-
-    const testimonials = [
-        {
-            client: 'Meredyth G. & Auggie',
-            highlight: 'Jaime helped me and my new best friend Auggie to get comfortalbe with commands and learn to really trust each other.',
-            reviewText: "Auggie particularly had a problem with pulling on the leash when I first brought him home, but together with Jaime's help, we're well on our way to being great on leash! Super grateful for the techniques Jaime has given me so far and the patience shown with me in learning them. I couldn't recommend them enough to help train you and your furry children!"
-        }
-    ]
 
     return (
         <Card sx={{ minWidth: 275, backgroundColor: `${theme.palette.primary.main}`}} >
@@ -22,13 +14,13 @@ function TestimonialCard() {
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             Testimonial
           </Typography>
-          <Typography variant="h5" component="div" color="theme.palette.primary">{testimonials[0].highlight}
+          <Typography variant="h5" component="div" color="theme.palette.primary">{highlight}
           </Typography>
           <Typography variant="body2" sx={{ m: 1.5 }}>
-            {testimonials[0].reviewText}
+            {reviewText}
           </Typography>
           <Typography sx={{ mt: 1.5, textAlign: 'right' }} color="text.secondary">
-            ~ {testimonials[0].client}
+            ~ {client}
           </Typography>
         </CardContent>
       </Card>
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -4,12 +4,30 @@ import TestimonialCard from '../../components/TestimonialCard';
 
 function Landing () {
 
+    const testimonials = [
+        {
+            client: 'Meredyth G. & Auggie',
+            highlight: 'Jaime helped me and my new best friend Auggie to get comfortalbe with commands and learn to really trust each other.',
+            reviewText: "Auggie particularly had a problem with pulling on the leash when I first brought him home, but together with Jaime's help, we're well on our way to being great on leash! Super grateful for the techniques Jaime has given me so far and the patience shown with me in learning them. I couldn't recommend them enough to help train you and your furry children!"
+        }
+    ]
+
     return (
         <>
             <Nav />
             <section style={{ marginTop: '80px' }}>
                 <h2>Landing</h2>
-                <TestimonialCard />
+                {testimonials.map((testimonial, index) => {
+                    return (
+                        <TestimonialCard 
+                            key={index}
+                            client={testimonial.client}
+                            highlight={testimonial.highlight}
+                            reviewText={testimonial.reviewText}
+                        />
+                    )
+                })
+            }
             </section>
         </>
     );
