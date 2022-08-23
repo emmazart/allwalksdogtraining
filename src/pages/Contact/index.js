72fa11e535435a341a20a2e7351917cb78419c43
Emma Olson <emma.olson794@gmail.com>
2022-08-22 20:18:30 -0500

    copy over form component from portfolio project

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../../sections/Header';
+import ContactForm from '../../components/ContactForm';
 
 function Contact ({ title }) {
 
@@ -7,6 +8,12 @@ function Contact ({ title }) {
         <>
         <Header />
         <h1>{title}</h1>
+        {/* FORM UNDER CONSTRUCTION ALERT */}
+            <div className='bg-tertiary p-2 w-9/12 rounded'>
+                <h3 className="text-md italic text-lightest">This form is currently under construction</h3>
+                <h4 className="text-sm italic text-lightest">To contact, please email: allwalksdogtraining@gmail.com</h4>
+            </div>
+        <ContactForm />
         </>
     )
 }
