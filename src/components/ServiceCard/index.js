b42667b81bbfe5b95cb877625a7375f15ee77723
Emma Olson <emma.olson794@gmail.com>
2022-08-23 17:08:14 -0500

    move services array to services section and pass props to cards

--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -10,36 +10,26 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { ListItemText } from "@mui/material";
 
-function ServiceCard() {
-  const theme = useTheme();
-
-  const services = [
-    {
-      title: "Ask Me Anything",
-      price: "$160",
-      description: "get some questions answered!",
-      bullets: [
-        "it comes with this thing",
-        "and this other thing",
-        "and this last thing",
-      ],
-    },
-  ];
+function ServiceCard({ title, price, description, bullets }) {
+  
+    const theme = useTheme();
+// handle asynchronysity of finding the props
+  if (!bullets) return null;
 
   return (
     <Card sx={{ m: 3, minWidth: 275, display: 'flex', justifyContent: 'center' }}>
       <CardContent>
         <Typography variant="h5" component="div">
-          {services[0].title}
+          {title}
         </Typography>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          {services[0].description}
+          {description}
         </Typography>
         <Typography sx={{ fontSize: 24 }}>
-          {services[0].price}
+          {price}
         </Typography>
         <List>
-          {services[0].bullets.map((bullet, index) => {
+          {bullets.map((bullet, index) => {
             return (
               <ListItem key={index} sx={{ py: '3px' }}>
                 <ListItemIcon> <PetsIcon /> </ListItemIcon>
--- src/sections/Services/index.js
+++ src/sections/Services/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import ServiceCard from '../../components/ServiceCard';
 
 function Services () {
 
+    const services = [
+        {
+          title: "Ask Me Anything",
+          price: "$160",
+          description: "get some questions answered!",
+          bullets: [
+            "it comes with this thing",
+            "and this other thing",
+            "and this last thing",
+          ],
+        },
+        {
+            title: "Ask Me Anything",
+            price: "$160",
+            description: "get some questions answered!",
+            bullets: [
+              "it comes with this thing",
+              "and this other thing",
+              "and this last thing",
+            ],
+          },
+      ];
+
     return (
         <section>
-            <h2>Services</h2>
+            <Typography component="h2" variant="h3">Services</Typography>
+            {services.map((service, index) => {
+                return (
+                    <ServiceCard key={index} title={service.title} price={service.price} description={service.description} bullets={service.bullets} />
+                )
+            })}
         </section>
     );
 }
